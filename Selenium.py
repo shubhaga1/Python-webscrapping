6c27791774205ff7de91fa46372ca322b74d7f2a
shubhaga1 <48895052+shubhaga1@users.noreply.github.com>
2023-07-12 08:46:45 +0530

    Update and rename selenium to Selenium.py

--- Selenium.py
+++ Selenium.py
@@ -1,10 +1,10 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-
 driver = webdriver.Chrome(ChromeDriverManager().install())
 driver.maximize_window()
 driver.get("https://indiacovidresources.in/full")
+
 #res = driver.find_elements_by_xpath("//*[@id='screenScrollView0']")
 res = driver.find_elements_by_xpath('//*[@id="all-cities"]')
 print(len(res))
